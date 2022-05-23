6b546fe22319614d85f63bbea7f7508a3b19ef22
Carolina Nonato <csnonato@gmail.com>
2022-05-22 22:47:42 -0400

    refectoring css: footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,15 +4,15 @@ import "../Styles/Footer.scss"
 
 function Footer() {
     return (
-        <div className='footer-component'>
-            <div className="footer-wrapper">
-                <h3 className='logo'>SP labs_</h3>
-                <div className="links-wrapper">
-                    <div className="splabs-links">
+        <div className='footer'>
+            <div className="footer__wrapper">
+                <h3 className='footer__logo'>SP labs_</h3>
+                <div className="footer__links-wrapper">
+                    <div className="footer__splabs-links">
                         <h4>SP Labs</h4>
                         <h5>Política de Privacidade</h5>
                     </div>
-                    <div className="sp-links">
+                    <div className="footer__sp-links">
                         <h4>Sem Processo</h4>
                         <a href="https://www.semprocesso.com.br/">Site</a>
                         <a href="https://www.semprocesso.com.br/#produtos">Produtos</a>
@@ -20,7 +20,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <p className="credits">SP Labs 2022. Feito por CAROLINA NONATO</p>
+            <p className="footer__credits">SP Labs 2022. Feito por CAROLINA NONATO</p>
         </div>
     )
 }
